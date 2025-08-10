ab595bebb7d1bf1f05cf43d60317eac186d2fcbe
ZhafranBahij <muhammadzhafranbahij@gmail.com>
2025-08-10 13:52:49 +0700

    insert node module

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,8 +1,8 @@
 <x-layouts.app>
 
     <div class="mb-6">
-        <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">{{ __('Dashboard')}}</h1>
-        <p class="text-gray-600 dark:text-gray-400 mt-1">{{ __('Welcome to the dashboard') }}</p>
+        <h1 class="text-2xl font-bold text-gray-800 dark:text-blue-400">{{ __('Yahallo')}}</h1>
+        <p class="text-gray-600 dark:text-gray-400 mt-1">{{ __('Welcome to the yahallo') }}</p>
     </div>
 
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 mb-6">
